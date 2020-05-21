7a6d9d1ede97e33810cc50d1127fd383e3eff2c4
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-05-21 22:12:56 +0300

    feat: add deck and images

---
+++ deck/ru/lib/theme.js
@@ -0,0 +1,11 @@
+export default {
+  fonts: {
+    body: 'Roboto, sans-serif',
+    monospace: '"Roboto Mono", monospace',
+  },
+  colors: {
+    text: 'white',
+    background: '#011627',
+    primary: '#addb67',
+  },
+}
