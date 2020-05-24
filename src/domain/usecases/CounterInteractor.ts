f0cc793b259dc03c94eb54472525daf570bd4ff0
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-05-24 08:44:50 +0300

    fix: test run error

--- src/domain/usecases/CounterInteractor.ts
+++ src/domain/usecases/CounterInteractor.ts
@@ -5,6 +5,7 @@ export interface CounterUseCaseIn {
   increment(): Promise<number>
 }
 
+// Required interface
 export interface CounterUseCaseOut {
   getCounter(): Promise<Counter>
   updateCounter(counter: Counter): Promise<Counter>
