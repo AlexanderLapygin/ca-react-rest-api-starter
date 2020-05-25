6a1c0f64fde274fd03b484029aca6d515a5fb305
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-05-26 00:42:48 +0300

    test: add useResetPresenter test

--- src/adapters/presenters/useResetPresenter.ts
+++ src/adapters/presenters/useResetPresenter.ts
@@ -1,38 +1,38 @@
-// import { useState, useCallback, useEffect } from 'react'
-// import { CounterIncrementIn } from '../../domain/usecases'
-//
-// export const useResetPresenter = (counterResetIn: CounterIncrementIn) => {
-//   const [counter, setCounter] = useState<number>()
-//
-//   useEffect(() => {
-//     ;(async (): Promise<void> => {
-//       try {
-//         const newCounter: number = await counterResetIn..getCounter()
-//         setCounter(0)
-//       } catch (error) {
-//         console.error(error)
-//       }
-//     })()
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [])
-//
-//   const handleIncrement = useCallback(async (): Promise<void> => {
-//     try {
-//       const newCounter: number = await counterResetIn.increment()
-//       setCounter(newCounter)
-//     } catch (error) {
-//       console.error(error)
-//     }
-//   }, [counterResetIn])
-//
-//   return {
-//     state: {
-//       counter
-//     },
-//     functions: {
-//       increment: handleIncrement
-//     }
-//   }
-// }
-//
-// export default useResetPresenter
+import { useState, useCallback, useEffect } from 'react'
+import { CounterResetIn } from '../../domain/usecases/CounterReset'
+
+export const useResetPresenter = (counterResetIn: CounterResetIn) => {
+  const [counter, setCounter] = useState<number>()
+
+  useEffect(() => {
+    ;(async (): Promise<void> => {
+      try {
+        await counterResetIn.reset()
+        setCounter(0)
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleReset = useCallback(async (): Promise<void> => {
+    try {
+      await counterResetIn.reset()
+      setCounter(0)
+    } catch (error) {
+      console.error(error)
+    }
+  }, [counterResetIn])
+
+  return {
+    state: {
+      counter
+    },
+    functions: {
+      reset: handleReset
+    }
+  }
+}
+
+export default useResetPresenter
